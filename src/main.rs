55fe012d7ccc215a445b8ccd00c458ab51eb0b56
Bloem <sem@ie-design.nl>
2025-07-21 23:22:33 +0200

    now prints the names of the tasks to the TUI

--- src/main.rs
+++ src/main.rs
@@ -24,20 +24,22 @@ use commands::{
 
 fn main()
 {
-    let cli: Cli = Cli::parse();
+    let mut cli: Cli = Cli::parse();
     let path: PathBuf = files::check_dir_valid().expect("failed in checking the dirs");
     println!("Hello, world! {}", path.display());
 
-    let data: Data = files::read_data(&cli);
+    let mut data: Data = files::read_data(&cli);
+
+    cli.tasks = Some(commands::tasks::TasksCli::NONE);
 
     if cli.tasks.is_some()
     {
-        tasks::run(&mut data.tasks.unwrap(), &cli.tasks.unwrap());
+        tasks::run(&mut data.tasks.as_mut().unwrap(), &cli.tasks.as_ref().unwrap());
     }
 
     let terminal: DefaultTerminal = ratatui::init();
     tui::tui::start();
-    tui::tui::run(terminal);
+    tui::tui::run(terminal, &mut data);
     ratatui::restore();
 }
 
--- src/tui/tui.rs
+++ src/tui/tui.rs
@@ -1,20 +1,28 @@
 use ratatui::{
-    crossterm::event::{self, Event}, widgets::{Paragraph, Widget}, DefaultTerminal, Frame
+    crossterm::event::{self, Event},
+    layout::{Constraint, Layout, Direction}, 
+    style::{Color, Stylize},
+    widgets::{self, Block, List, ListItem, Paragraph, Widget},
+    prelude::{Rect},
+    DefaultTerminal,
+    Frame
 };
+
+use crate::structs::Data;
 
 pub fn start()
 {
     color_eyre::install().unwrap();
 }
 
-pub fn run(mut terminal: DefaultTerminal)
+pub fn run(mut terminal: DefaultTerminal, mut data: &mut Data)
 {
-    // rendering
-    terminal.draw(render).unwrap();
-
-    // input handeling
     'main_render_loop: loop
     {
+        // rendering
+        terminal.draw(|frame| render(frame, data)).unwrap();
+
+        // input handeling
         if let Event::Key(key) = event::read().unwrap()
         {
             match key.code
@@ -26,7 +34,34 @@ pub fn run(mut terminal: DefaultTerminal)
     }
 }
 
-fn render(frame: &mut Frame)
+fn render(frame: &mut Frame, mut data: &Data)
 {
-    Paragraph::new("Hello ma'am :D").render(frame.area(), frame.buffer_mut());
+    let chunks: [Rect; 1] = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Fill(1)].as_ref())
+        .areas(frame.area());
+
+    let border_area: Rect = chunks[0];
+
+    let [inner_area] = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Fill(1)].as_ref())
+        .areas(border_area);
+
+    Block::bordered().border_type(widgets::BorderType::Rounded).
+        fg(Color::Magenta).
+        render(border_area, frame.buffer_mut());
+
+
+    List::new(data.tasks.as_ref().unwrap().tasks
+            .iter()
+            .map(|x| ListItem::from(x.task.clone()))
+        )
+        .render(inner_area, frame.buffer_mut());
+
+
+    Paragraph::new("Hello World! :D").render(frame.area(), frame.buffer_mut());
+
 }
